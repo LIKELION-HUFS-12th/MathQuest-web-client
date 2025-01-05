ffcceef01f753b204c539139f3e104dcf94ac0f1
[JInsunnya] <[jsk5754@naver.com]>
2025-01-05 19:39:01 +0900

    웹앱 화면값 설정

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+// import * as Q from "../styles/LandingStyles";
+
+const Quest = () => {
+  return <p>랜딩 페이지</p>;
+};
+export default Quest;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import * as M from '../styles/MainStyles';
+
+const Main = () => {
+  return (
+    <M.Container>
+      <p>퀘스트 목록</p>
+    </M.Container>
+  );
+};
+export default Main;
